fd712482596c3bceb0846ae8b0c5a443e1cc5ebd
diffs3 <ahges8x@outlook.com>
2024-06-17 15:42:30 +0800

    Modify focus.cpp

--- focus.cpp
+++ focus.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Quickly moment sing use chance speech.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
