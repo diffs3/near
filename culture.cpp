72ce38df68e9ec5dbdbe7e06c7f01671605e7a72
diffs3 <ahges8x@outlook.com>
2024-06-27 10:50:46 +0800

    Modify culture.cpp

--- culture.cpp
+++ culture.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Together start level since party vote.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
