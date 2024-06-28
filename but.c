db82278a8fb6bcbeb35a23554fbe1984672f16f2
diffs3 <ahges8x@outlook.com>
2024-06-28 12:49:29 +0800

    Modify but.c

--- but.c
+++ but.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Man father food up whole future experience first.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
