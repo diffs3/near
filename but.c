e0d3872ec8c4bd24d574037906c638e72a982569
diffs3 <ahges8x@outlook.com>
2024-07-01 14:34:20 +0800

    Modify but.c

--- but.c
+++ but.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Movement artist responsibility rise under.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
