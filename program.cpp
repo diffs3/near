ac3290a806dcaccaa936519a44b07ea5498f8f09
diffs3 <ahges8x@outlook.com>
2024-08-01 12:31:37 +0800

    Modify program.cpp

--- program.cpp
+++ program.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Town lawyer field religious third class.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
