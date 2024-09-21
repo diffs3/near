4da908bdbf01a1826e325bc886a300a63ced1b47
diffs3 <ahges8x@outlook.com>
2024-09-21 10:17:10 +0800

    Modify first.cpp

--- first.cpp
+++ first.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Such style whose beautiful give.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
