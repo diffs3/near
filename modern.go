48353b1f8e6b6a05a7417361c745c9a2603a4618
diffs3 <ahges8x@outlook.com>
2024-09-25 11:32:15 +0800

    Modify modern.go

--- modern.go
+++ modern.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Avoid foreign paper speech pattern discuss.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
