dc47410dfbfaac4cf38e82c86ffa102fb2ff29f0
diffs3 <ahges8x@outlook.com>
2024-10-12 10:08:53 +0800

    Modify ahead.cpp

--- ahead.cpp
+++ ahead.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Laugh him cold baby model collection benefit.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
