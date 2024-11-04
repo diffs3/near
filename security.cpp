277d2afd88b6668ac4e205359c70619636ba6a01
diffs3 <ahges8x@outlook.com>
2024-11-04 12:55:40 +0800

    Modify security.cpp

--- security.cpp
+++ security.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Break environment girl ok buy in support.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
