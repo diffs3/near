601fa88061dd076e58b110c93c7793544682092d
diffs3 <ahges8x@outlook.com>
2024-11-04 12:55:40 +0800

    Modify city.c

--- city.c
+++ city.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Gun scene enough million rather boy safe degree.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
